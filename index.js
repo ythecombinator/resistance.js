eb3eebe41dbccd6ff9a6c4fe0f3cd445acc96327
Matheus Brasil <matheus.brasil10@gmail.com>
2016-02-10 22:38:59 -0200

    Compile source

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+var ref$, map, fold1;
+ref$ = require('prelude-ls'), map = ref$.map, fold1 = ref$.fold1;
+module.exports = function(circuit, type){
+  var series, parallel;
+  series = function(circuit){
+    return fold1(curry$(function(x$, y$){
+      return x$ + y$;
+    }), circuit);
+  };
+  parallel = function(circuit){
+    return (function(it){
+      return Math.pow(it, -1);
+    })(fold1(curry$(function(x$, y$){
+      return x$ + y$;
+    }), map((function(it){
+      return Math.pow(it, -1);
+    }), circuit)));
+  };
+  switch (type) {
+  case 'series':
+  case 's':
+    return series(circuit);
+  case 'parallel':
+  case 'p':
+    return parallel(circuit);
+  default:
+    return console.log('Please enter a valid type of resistors association.');
+  }
+};
+function curry$(f, bound){
+  var context,
+  _curry = function(args) {
+    return f.length > 1 ? function(){
+      var params = args ? args.concat() : [];
+      context = bound ? context || this : this;
+      return params.push.apply(params, arguments) <
+          f.length && arguments.length ?
+        _curry.call(context, params) : f.apply(context, params);
+    } : f;
+  };
+  return _curry();
+}
